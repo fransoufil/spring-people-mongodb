2e2f2dc4dabd99e32dbcab71855a15eea839cc40
fransoufil <francixco.sf@gmail.com>
2021-05-17 16:40:36 -0300

    Get People by Id with Error Implement

--- src/main/java/com/fransoufil/people/resources/PeopleResource.java
+++ src/main/java/com/fransoufil/people/resources/PeopleResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,12 @@ public class PeopleResource {
 		List<People> list = peopleService.findAll();
 		List<PeopleDTO> listDto = list.stream().map(x -> new PeopleDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<PeopleDTO> findById(@PathVariable String id) throws Exception {
+		People obj = peopleService.findById(id);
+		return ResponseEntity.ok().body(new PeopleDTO(obj));
 	}
 
 }
--- src/main/java/com/fransoufil/people/services/PeopleService.java
+++ src/main/java/com/fransoufil/people/services/PeopleService.java
@@ -1,12 +1,14 @@
 package com.fransoufil.people.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fransoufil.people.domain.People;
 import com.fransoufil.people.repositories.PeopleRepository;
+import com.fransoufil.people.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PeopleService {
@@ -17,5 +19,9 @@ public class PeopleService {
 	public List<People> findAll(){
 		return peopleRepository.findAll();
 	}
-
+	
+	public People findById(String id) {
+		Optional<People> obj = peopleRepository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("People with Id: " + id + " not found!"));
+	}
 }
